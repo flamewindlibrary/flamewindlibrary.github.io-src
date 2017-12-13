572bb1910e640ed7601edd8545e3ed6eb99423ae
maxicecilia <maxicecilia@gmail.com>
2017-12-13 02:43:30 -0300

    Initial setup

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *  # noqa
 
-SITEURL = 'https://thekodaxlibrary'
+SITEURL = 'https://flamewindlibrary.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
